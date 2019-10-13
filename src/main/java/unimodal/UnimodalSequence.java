f846425cf9d4ee13656aa0b0a8457befc4b7c164
Sathish Gopalakrishnan <sathish@gmail.com>
2019-10-12 17:06:50 -0700

    Initial commit

---
+++ src/main/java/unimodal/UnimodalSequence.java
@@ -0,0 +1,19 @@
+package unimodal;
+
+public class UnimodalSequence {
+
+	/**
+	 * Determine the length of the longest unimodal sequence in intArray
+	 * 
+	 * @param intArray
+	 *            is not null
+	 * @return the length of the longest unimodal sequence in intArray
+	 * @throws NoUnimodalSequenceException
+	 *             if there is no unimodal sequence in intArray
+	 */
+	public static int getLength_longestUnimodalSequence(int[] intArray) throws NoUnimodalSequenceException {
+		// TODO implement this method
+		return 0; // change this
+	}
+
+}
